9d03c6f10e1eaaed2d73ff6680d9aa237a033887
Pabitra Bera <126568738+madeeasycodinglife@users.noreply.github.com>
2023-06-29 08:26:59 +0530

    Add files via upload

---
+++ spring-boot-with-ssl/src/main/java/com/madeeasy/SpringBootWithSslApplication.java
@@ -0,0 +1,13 @@
+package com.madeeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSslApplication.class, args);
+	}
+
+}
